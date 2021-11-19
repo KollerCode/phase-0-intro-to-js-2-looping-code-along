9020c90d0cf12a22e3bd5bb175443452e9635723
Natasha Koller <kollernatasha@gmail.com>
2021-11-19 11:56:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+    
+function countDown(positiveNumber) {
+    let i = 0;
+    while (positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+    
+}
